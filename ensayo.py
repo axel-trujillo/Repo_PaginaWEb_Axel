ddc5e316cf07ef7a90f97e469f9efa5e63001755
axel-trujillo <axeltrujillo1110@gmail.com>
2026-05-03 16:33:22 -0400

    mi web

---
+++ ensayo.py
@@ -0,0 +1,17 @@
+import random
+mis_pokemons = ["Rayquaza", "Gengar"]
+pokemons_rival =["Garchomp", "Hydreigon", "Pikachu", "Dedenne","Rattata", "Pidgey"]
+
+vida_Rayquaza = 351
+vida_Gengar = 261
+vida_Garchomp = 357
+vida_Hydreigon = 325
+vida_Pikachu = 35
+vida_Dedenne = 67
+vida_Rattata = 30
+vida_pidgey = 40
+while (vida_Rayquaza + vida_Gengar) > 0:
+    pokemon_elegido = input(f"¿Cuál pokemon quieres sacar? {len(mis_pokemons)}")
+    if pokemon_elegido == Rayquaza :
+        print(f"Yo te elijo Rayquaza")
+        break
